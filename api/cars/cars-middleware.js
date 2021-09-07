19bba60fa7d3b44b2e70b85832c81b3f37abeea0
JoeKrapsicher <JoeKrapsicher@gmail.com>
2021-09-06 18:30:25 -0600

    Finished Assignment

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,65 @@
+const Cars = require("./cars-model")
+var vinValidator = require('vin-validator');
+
+
+module.exports = {
+  checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique
+}
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params
+  const car = Cars.getById(id)
+
+  if (car) {
+    req.car = car;
+    res.status(200);
+    next();
+  } else {
+    res.status(404).json({message: `Car with id ${id} not found`})
+  }
+   
 }
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  const field = Cars.getById(id)
+
+  if (field) {
+    req.field = field;
+    res.status(200);
+    next();
+  } else {
+    res.status(400).json({message: `${field} is missing`})
+  }
+  
 }
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  const vin = Cars.getById(id);
+
+  if (vin) {
+    req.vin = vin;
+    res.status(200);
+    next();
+  } else {
+    res.satus(400).json({message: `${vin} is invalid`})
+  }
 }
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  const vin = Cars.getById(id);
+
+  if (vin) {
+    req.vin = vin;
+    res.status(200);
+    next();
+  } else {
+    res.status(400).json({message: `vin ${vin} already exists`})
+  }
 }
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
+const db = require('../../data/db-config')
+
+module.exports = {
+  getAll, getById, create
+}
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  return db('cars')
+    .where({ id })
+    .first();
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (cars) => {
+  return db('cars')
+    .insert(cars)
+    .then(ids => {
+      return getById(ids[0]);
+    })
 }
